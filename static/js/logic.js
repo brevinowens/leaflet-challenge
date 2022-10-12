7ac1865b1c16350d606870e828b0958d4b765adc
Brevin Owens <brevin.owens@gmail.com>
2022-10-12 18:27:47 -0500

    updating for resubmit

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,6 +10,7 @@ let myMap = L.map("map", {
     layers: [street]
 });
 
+const chooseColor = depth => depth<10 ? 'lightgreen' : depth<30 ? 'green' : depth<50 ? 'yellow' : depth<70 ? 'orange' : depth<90 ? 'darkorange' : 'red';
 
 // Creating a variable which holds our query url
  let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson"
@@ -19,19 +20,21 @@ d3.json(queryUrl).then(({features})=> {
         let { coordinates } = feature.geometry;
         let { mag, place } = feature.properties;
         //creating circles to plot each earthquake
-        L.circle([coordinates[1],coordinates[0]],{radius: mag*10000, color: 'black', fillColor: chooseColor(feature.properties.mag), fillOpacity: [coordinates[2]]}).addTo(myMap).bindPopup(place + "<br>" + "Magnitude:" + mag + "<br>" + "Depth:" + [coordinates[2]]);
+        L.circle([coordinates[1],coordinates[0]],{radius: mag*20000, color: 'black', fillColor: chooseColor(coordinates[2]), fillOpacity: .65 }).addTo(myMap).bindPopup(place + "<br>" + "Magnitude:" + mag + "<br>" + "Depth:" + [coordinates[2]]);
     });
 });
 
 //creating a function by which the fill color of the circle will change based on the magnitude of the earthquake
-function chooseColor(mag) {
-    if (mag >= 0 & mag <= 1) return "lightgreen";
-    else if (mag > 1 & mag <= 3) return "green";
-    else if (mag > 3 & mag <= 5) return "yellow";
-    else if (mag > 5 & mag <= 7) return "orange";
-    else if (mag > 7 & mag <= 9) return "darkorange";
-    else return "red";
-  };
+
+
+// function chooseColor(depth) {
+//     if (depth >= 0 & depth <= 1) return "lightgreen";
+//     else if (depth > 1 & depth <= 3) return "green";
+//     else if (depth > 3 & depth <= 5) return "yellow";
+//     else if (depth > 5 & depth <= 7) return "orange";
+//     else if (depth > 7 & depth <= 9) return "darkorange";
+//     else return "red";
+//   };
 
  //creating a legend
  var legend = L.control({
@@ -40,16 +43,19 @@ function chooseColor(mag) {
   
 legend.onAdd = function(){
     var div = L.DomUtil.create("div", "legend");
-    this.update()
+    var categories = ['<10','<30','<50', '<70', '<90', '90+'];
+    var colors = ["lightgreen", "green", "yellow", "orange", "darkorange", "red"];
+
+    div.innerHTML = "<h4>Depth</h4>"
+
+    categories.forEach((cat,i) => {
+        div.innerHTML += `<i class="${colors[i]}">${cat}</i>`
+    });
+
     return div;
 };
 
-legend.update = function (){
-    labels = chooseColor(mag)
-    categories = ['0-1','1-3','3-5','5-7','7-9','9+']
-};
-
-  // Add the info legend to the map.
+// Add the info legend to the map.
 legend.addTo(myMap);
 
 
